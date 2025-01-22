481f3075e47c927e00e44d2be59c0b0fa6c67b3c
Ondřej Bakan <ondrej@bakan.cz>
2025-01-22 14:19:52 +0100

    Refactor for endianness, fixed error (bytes vs bits), added position() function.

--- src/ByteBuffer.php
+++ src/ByteBuffer.php
@@ -5,34 +5,61 @@ namespace OndrejBakan\ByteBuffer;
 class ByteBuffer
 {
     private $stream;
-    private $orderLittleEndian = true;
+    private string $endianness;
 
-    public function __construct($data)
+    const LITTLE_ENDIAN = 'little';
+    const BIG_ENDIAN = 'big';
+
+    public function __construct($data, $endianness = SELF::LITTLE_ENDIAN)
     {
         $this->stream = fopen('php://memory', 'r+');
+        $this->endianness = $endianness;
+
+        if (!is_string($data))
+            $data = strval($data);
 
         fwrite($this->stream, $data);
         rewind($this->stream);
     }
 
-    public function read($length)
+    public function position(): int|false
+    {
+        return ftell($this->stream);
+    }
+
+    public function read(int $length = 1): string|false
     {
         return fread($this->stream, $length);
     }
 
-    public function readUint16()
+    public function readUint16(): int
     {
-        return unpack($this->orderLittleEndian ? 'v' : 'n', $this->read(16))[1];
+        return $this->unpackData('v', 'n', $this->read(2));
     }
 
-    public function readUint32()
+    public function readUint32(): int
     {
-        return unpack($this->orderLittleEndian ? 'V' : 'N', $this->read(32))[1];
+        return $this->unpackData('V', 'N', $this->read(4));
     }
 
-    public function readUint64()
+    public function readUint64(): int
     {
-        return unpack($this->orderLittleEndian ? 'P' : 'J', $this->read(64))[1];
+        return $this->unpackData('P', 'J', $this->read(8));
+    }
+
+    public function seek(int $offset, int $whence = SEEK_SET)
+    {
+        fseek($this->stream, $offset, $whence);
+    }
+
+    public function skip(int $offset)
+    {
+        fseek($this->stream, $offset, SEEK_CUR);
+    }
+
+    private function unpackData($littleEndianFormat, $bigEndianFormat, $data)
+    {
+        return unpack($this->endianness === self::LITTLE_ENDIAN ? $littleEndianFormat : $bigEndianFormat, $data)[1];
     }
 
 }
--- tests/ByteBufferTest.php
+++ tests/ByteBufferTest.php
@@ -5,6 +5,14 @@ use PHPUnit\Framework\TestCase;
 class ByteBufferTest extends TestCase
 {
 
+    public function testReadInt16()
+    {
+        $data = pack('s', 65535);
+        $buffer = new OndrejBakan\ByteBuffer\ByteBuffer($data);
+
+        $this->assertEquals(65535, $buffer->readUint16());
+    }
+
     public function testReadUint16()
     {
         $data = pack('v', 65535);
@@ -26,6 +34,16 @@ class ByteBufferTest extends TestCase
         $data = pack('P', 9223372036854775807);
         $buffer = new OndrejBakan\ByteBuffer\ByteBuffer($data);
 
+        $this->assertEquals(9223372036854775807, $buffer->readUint64());
+    }
+
+    public function testReadMultiple()
+    {
+        $data = pack('vVP', 65535, 4294967295, 9223372036854775807);
+        $buffer = new OndrejBakan\ByteBuffer\ByteBuffer($data);
+
+        $this->assertEquals(65535, $buffer->readUint16());
+        $this->assertEquals(4294967295, $buffer->readUint32());
         $this->assertEquals(9223372036854775807, $buffer->readUint64());
     }
 }
